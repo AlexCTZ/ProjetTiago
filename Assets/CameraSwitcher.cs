a6a56fc52daa2c506aee6c3669cd78b01dfa67c9
AlexCTZ <alex.caratza@gmail.com>
2025-10-02 11:15:32 +0200

    La fly cam marche

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera xrCamera;
+    public Camera FlyCamera;
+
+    private bool usingSpectator = false;
+
+    void Start()
+    {
+        // Assure que les deux caméras sont actives
+        xrCamera.gameObject.SetActive(true);
+        FlyCamera.gameObject.SetActive(true);
+
+        // XR par défaut sur le PC
+        xrCamera.depth = 1;
+        FlyCamera.depth = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            usingSpectator = !usingSpectator;
+
+            if (usingSpectator)
+            {
+                // Spectator prend la priorité sur le PC
+                FlyCamera.depth = 2;
+                xrCamera.depth = 1;
+            }
+            else
+            {
+                // XR reprend la priorité
+                xrCamera.depth = 2;
+                FlyCamera.depth = 1;
+            }
+        }
+    }
+}
